91522bb1928655a1aa0126f0c0505027a1b7b736
yujin Park <parkyu@adenasoft.com>
2022-08-12 18:41:25 +0900

    feat: useEffect 이론 및 사용법

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import { useState, useEffect } from "react";
+
+// interface Props {
+//   event:any;
+// }
+
 function App() {
-  return <div className="App">nomad test</div>;
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time(매번실행)");
+
+  const onChange = (event:any) => setKeyword(event.target.value); 
+  
+  useEffect(() => {
+    console.log("call the api....")
+  },[]);
+
+  useEffect(() => {
+  if( keyword !== "" && keyword.length >= 5){
+    console.log("Search for",keyword);
+  }
+  },[keyword]);
+
+  return (
+  <div className="App">
+    <input value={keyword} onChange={onChange} type="text" placeholder="Search here" />
+    <h1>{counter}</h1>
+    <button onClick={onClick}>Click me!</button>
+  </div>
+  );
+  
 }
 
 export default App;
