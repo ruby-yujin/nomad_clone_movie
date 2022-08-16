986be599f06ea523fc922e45a3c4a560ec8ec515
yujin Park <parkyu@adenasoft.com>
2022-08-16 17:08:22 +0900

    feat: useEffect 챕터 6 완강

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,56 @@ import { useState, useEffect } from "react";
 //   event:any;
 // }
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
+function Hello(){
 
-  const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time(매번실행)");
-
-  const onChange = (event:any) => setKeyword(event.target.value); 
-  
-  useEffect(() => {
-    console.log("call the api....")
+  useEffect(function() {
+      console.log('hi');
+      return function() {
+        console.log('bye -_-')
+      };
   },[]);
 
-  useEffect(() => {
-  if( keyword !== "" && keyword.length >= 5){
-    console.log("Search for",keyword);
-  }
-  },[keyword]);
 
-  return (
-  <div className="App">
-    <input value={keyword} onChange={onChange} type="text" placeholder="Search here" />
-    <h1>{counter}</h1>
-    <button onClick={onClick}>Click me!</button>
-  </div>
-  );
+  return <h1>Hello</h1>
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+    return (
+    <div className="App">
+      {showing ? <Hello/> : null}
+      <button onClick={onClick}>{showing ?  "Hide" : "show"}</button>
+    </div>
+    );
+  // const [counter, setValue] = useState(0);
+  // const [keyword, setKeyword] = useState("");
+
+  // const onClick = () => setValue((prev) => prev + 1);
+  // console.log("i run all the time(매번실행)");
+
+  // const onChange = (event:any) => setKeyword(event.target.value); 
+  
+  // useEffect(() => {
+  //   console.log("call the api....")
+  // },[]);
+
+  // //keyword 가 업데이트될때! 함수 실행되라
+  // useEffect(() => {
+  // console.log("i run when 'keyword' change");
+  // },[keyword]);
+  
+  // //counter 가 업데이트될때! 함수 실행되라!
+  // useEffect(()=>{
+  //   console.log("i run when 'Counter' change");
+  // },[counter])
+  
+  // // keyword, counter 가 업데이트 될때 함수 실행되라!
+  // useEffect(() => {
+  //   console.log(" i run when 'keyword' && 'conter'");
+  // },[keyword,counter])
+
+
   
 }
 
