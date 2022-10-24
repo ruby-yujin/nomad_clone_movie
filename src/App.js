bcd39b2a230753710a3c6b21542d190f0b6d9990
yujin Park <parkyu@adenasoft.com>
2022-10-24 18:59:55 +0900

    Todo list , Coin list

--- src/App.js
+++ src/App.js
@@ -1,39 +1,21 @@
-import { useState, useEffect } from "react";
-
-// interface Props {
-//   event:any;
-// }
-
-function Hello(){
-
-  useEffect(function() {
-      console.log('hi');
-      return function() {
-        console.log('bye -_-')
-      };
-  },[]);
-
-
-  return <h1>Hello</h1>
-}
+import React from "react";
+import CoinList from "./CoinList";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-    return (
-    <div className="App">
-      {showing ? <Hello/> : null}
-      <button onClick={onClick}>{showing ?  "Hide" : "show"}</button>
-    </div>
-    );
+  return (
+    <>
+      <CoinList />
+    </>
+  );
+
   // const [counter, setValue] = useState(0);
   // const [keyword, setKeyword] = useState("");
 
   // const onClick = () => setValue((prev) => prev + 1);
   // console.log("i run all the time(매번실행)");
 
-  // const onChange = (event:any) => setKeyword(event.target.value); 
-  
+  // const onChange = (event:any) => setKeyword(event.target.value);
+
   // useEffect(() => {
   //   console.log("call the api....")
   // },[]);
@@ -42,19 +24,16 @@ function App() {
   // useEffect(() => {
   // console.log("i run when 'keyword' change");
   // },[keyword]);
-  
+
   // //counter 가 업데이트될때! 함수 실행되라!
   // useEffect(()=>{
   //   console.log("i run when 'Counter' change");
   // },[counter])
-  
+
   // // keyword, counter 가 업데이트 될때 함수 실행되라!
   // useEffect(() => {
   //   console.log(" i run when 'keyword' && 'conter'");
   // },[keyword,counter])
-
-
-  
 }
 
 export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+function TodoList() {
+  const [toDo, setTodo] = useState("");
+  const [toDos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setTodo("");
+    setTodos((currencyArray) => [toDo, ...currencyArray]);
+  };
+  console.log(toDos);
+  return (
+    <div>
+      <h1>My to Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>add to Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
